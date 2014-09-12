414f41cdbdca8281199661e58f19829f4d22bc22
Elliott OHara <elliottohara@Elliotts-MacBook-Pro.local>
2014-09-12 06:20:49 -0500

    move db to tmp for production

--- sbjj/settings.py
+++ sbjj/settings.py
@@ -60,7 +60,8 @@ DATABASES = {
 
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': '/tmp/db.sqlite3'
         }
 }
 
