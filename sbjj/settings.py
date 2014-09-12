e8c3f46d3734d0f6e5c2c5d0d1c31082c2f78336
Elliott OHara <elliottohara@Elliotts-MacBook-Pro.local>
2014-09-12 08:26:29 -0500

    adding production settings

--- sbjj/settings.py
+++ sbjj/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'mq-zw4#a*xm_mnhyy%1anu6ld@-u9juevbkdg7(&)qsl2_ts#i'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -55,26 +52,6 @@ WSGI_APPLICATION = 'sbjj.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': '/tmp/db.sqlite3'
-        }
-}
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': 'sbjj',
-#        'USER': 'sbjj',
-#        'PASSWORD': 'sbjj',
-#        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-#        'PORT': '3306',
-#    }
-#}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
@@ -110,3 +87,8 @@ STATIC_URL = '/static/'
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
 )
+
+if os.environ.get('DEVELOPMENT', None):
+    from settings_dev import *
+else:
+    from settings_prod import *
